15c97ef598330a670e7ab7466665b6800972a3d5
Help3rL <today9432@gmail.com>
2022-10-05 16:23:52 +0300

    formating

--- tsredux-burgerbuilder/src/App.tsx
+++ tsredux-burgerbuilder/src/App.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './assets/images/logo.png';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import Toolbar from './layout/toolbar/toolbar'
-import cartIcon from './assets/icons/Cart_icon.svg';
-import {createBrowserRouter, RouterProvider, Route, Outlet, Link} from 'react-router-dom';
+import React from "react";
+import logo from "./assets/images/logo.png";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import Toolbar from "./layout/toolbar/toolbar";
+import cartIcon from "./assets/icons/Cart_icon.svg";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+  Link,
+} from "react-router-dom";
 
-
-
-const navLinks ={
-  Home: '/',
-  Builder: '/builder',
-  Account: '/account',
-  Cart: '/account/cart',
-}
+const navLinks = {
+  Home: "/",
+  Builder: "/builder",
+  Account: "/account",
+  Cart: "/account/cart",
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Toolbar links={navLinks} logo={logo}/>
+        <Toolbar links={navLinks} logo={logo} />
       </header>
       <div>
         <img src={logo} className="App-logo" alt="logo" />
--- tsredux-burgerbuilder/src/controls/utility.tsx
+++ tsredux-burgerbuilder/src/controls/utility.tsx
@@ -1,41 +1,41 @@
 export interface UpdatedIngredientInterface {
-    type ?: string
-    ingredients?: any
-    error ?: boolean | null
-    purchased ?: boolean
-    loading ?: boolean
-    orders ?: any[]
-    id ?: number
-    idToken?: any
-    token ?: any
-    userId ?: any
-    building ?: boolean
-    authRedirectPath ?: string
-    path ?: string
-    totalPrice ?: number
-    valid ?: boolean
-    value ?: string
-    touched ?: boolean
+  type?: string;
+  ingredients?: any;
+  error?: boolean | null;
+  purchased?: boolean;
+  loading?: boolean;
+  orders?: any[];
+  id?: number;
+  idToken?: any;
+  token?: any;
+  userId?: any;
+  building?: boolean;
+  authRedirectPath?: string;
+  path?: string;
+  totalPrice?: number;
+  valid?: boolean;
+  value?: string;
+  touched?: boolean;
 }
 
-export const updateObj = (oldObj:Array<any>, objUpdates:Array<any>) => {
-    return({...oldObj, ...objUpdates});
-}
+export const updateObj = (oldObj: Array<any>, objUpdates: Array<any>) => {
+  return { ...oldObj, ...objUpdates };
+};
 
-export const checkValidity = (value:string , rules:any):boolean => {
-    let isValid:boolean = true
-    if(!rules){
-        return true;
-    }
-    if (rules.required) {
-        isValid = value.trim() !== '' && isValid;
-    }
-    if (rules.minLength) {
+export const checkValidity = (value: string, rules: any): boolean => {
+  let isValid: boolean = true;
+  if (!rules) {
+    return true;
+  }
+  if (rules.required) {
+    isValid = value.trim() !== "" && isValid;
+  }
+  if (rules.minLength) {
     isValid = value.length >= rules.minLength && isValid;
-    }   
-    if (rules.maxLength) {
-        isValid = value.length <= rules.maxLength && isValid;
-    }
-  
-    return isValid
-}
+  }
+  if (rules.maxLength) {
+    isValid = value.length <= rules.maxLength && isValid;
+  }
+
+  return isValid;
+};
