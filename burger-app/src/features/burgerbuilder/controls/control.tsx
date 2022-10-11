fa9b75ceca5c97191feec0362dbf6876d3917df2
Help3rL <68181784+Help3rL@users.noreply.github.com>
2022-10-11 16:12:55 +0300

    minor update

--- burger-app/src/features/burgerbuilder/controls/control.tsx
+++ burger-app/src/features/burgerbuilder/controls/control.tsx
@@ -1,21 +1,37 @@
 import React from "react";
+let hold = 1;
 export default function control(props:any){
-    const generator = (hen:Array<any>) =>{
-        for (let [key , value] of hen){
-            console.log(key + ':' + value)
+    const holdd = (action:string) => {
+        if (action === 'Add' && hold < 100){
+            hold++;
+        }else if (action === 'Substract' && hold > 0){
+            hold--;
+        }else{
+            console.log("MAX/MIN" + hold)
         }
-        return <a href="#">Test</a>
+        return undefined
+    }
+    const generator = (hen:any) =>{
+        const tempHold = []
+        for(const [key, value] of Object.entries(hen)){
+            console.log(key + ' : ' + value)
+            tempHold.push(
+                <div key={key + value}>
+                    <label htmlFor="buttons">{key} : {(Number(value)/100).toFixed(2)}</label>
+                    <div id="buttons">
+                        <button className="more" onClick={holdd('Add')}>More</button>
+                        <button className="less" onClick={holdd('Substract')}>Less</button>
+                    </div>
+                    <input type="number" name="amount" id="" min={0} max={99} step={1} defaultValue={hold}/>
+                </div>
+                 )
+        }
+        return tempHold
     }
 
     return(
         <div className='control'>
-            <label htmlFor="buttons">cost{generator(props.list)}</label>
-            <div id="buttons">
-                <button className="more">More</button>
-                <button className="less">Less</button>
-                
-            </div>
-            <input type="number" name="amount" id="" min={0} max={99} step={1} defaultValue={1}/>
+            {generator(props.list)}
         </div>
     )
 }
