c4b550c2161fc980602855333f5cf74746714f29
Help3rL <68181784+Help3rL@users.noreply.github.com>
2022-11-04 16:05:28 +0200

    Failiure but closer to understaing redux

--- Testing/my-app/src/controller/reduxbuttons.tsx
+++ Testing/my-app/src/controller/reduxbuttons.tsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
-
+import { resetInt, addint, reduceInt } from "../Redux/actions/actions";
+import { store } from "../Redux/store/store";
 export const Component = (props: any) => {
   const [counter, setCounter] = useState();
-
+  const resetCounter = store.dispatch(resetInt(1));
+  const addCounter = store.dispatch(addint(1))
+  const reduceCounter = store.dispatch(reduceInt(1))
   return (
     <div>
-      <button>Reset</button>
-      <button>Add 1</button>
-      <button>Reduce 1</button>
+      <button onClick={() => resetCounter}>Reset</button>
+      <button onClick={() => addCounter}>Add 1</button>
+      <button onClick={() => reduceCounter}>Reduce 1</button>
     </div>
   );
 };
